9d47a84a147b239c506668cb8a2c950104910cd9
Kirill Korolev <kkorolev@gmail.com>
2023-01-11 17:19:45 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let x = n;
+  if (n < 0) {
+    x = x * -1; // convert to positive dec
+  }
+  x = x.toString() //convert dec to string
+  x = x.split('') // convert string to array
+  x = x.reverse(); // reverse the array
+  x = x.join(''); // convert array to string
+  x = Number(x); // convert string to dec
+  return x;
 }
